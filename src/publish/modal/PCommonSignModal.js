7b066020cb9e4aa15d2ed7c01c0f88682e6134df
yamdeng <yamdeng@gmail.com>
2021-08-25 16:32:09 +0900

    sign 라이브러리 추가

--- src/publish/modal/PCommonSignModal.js
+++ src/publish/modal/PCommonSignModal.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { observer, inject } from 'mobx-react';
+import SignatureCanvas from 'react-signature-canvas';
 
 @withRouter
 @inject('modalStore')
@@ -16,11 +17,19 @@ class PCommonSignModal extends React.Component {
     this.props.modalStore.hideModal();
   }
 
+  componentDidMount() {}
+
   render() {
     return (
       <div className="popup-container size-330">
         <div className="popup-content">
           <strong>18 : </strong> 공통 결재 팝업
+        </div>
+        <div>
+          <SignatureCanvas
+            penColor="green"
+            canvasProps={{ width: 500, height: 200, className: 'sigCanvas' }}
+          />
         </div>
         <div className="popup-footer">
           <div className="btn-group wide">
