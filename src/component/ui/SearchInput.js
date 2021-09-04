5c953cf031bee8cc1f455d3bbc2da334da93bbbe
yamdeng <yamdeng@gmail.com>
2021-09-05 07:30:37 +0900

    searchInput 변경

--- src/component/ui/SearchInput.js
+++ src/component/ui/SearchInput.js
@@ -5,15 +5,13 @@ import SearchInputClear from './SearchInputClear';
 /*
 
     검색 필드 공통
-     : <SearchInput inputId='' value={''} label='' clearInput={() => {}} changeValue={() => {}} className='' />
+     : <SearchInput inputId='' value={''} label='' changeValue={() => {}}/>
 
     props
      -inputId(option) : 라벨을 매핑시키기 위한 id(없으면 uuid로 정의)
      -value : input value(값 여부에 따라 버튼 보이고 않보이게
      -label : input label
-     -clearV : [x] 버튼 handler 함수
      -changeValue : input 값 변경 handler 함수
-     -className(option) : 클래스명 변경 필요시에 사용
 
 */
 
@@ -24,7 +22,7 @@ class SearchInput extends React.Component {
   }
 
   render() {
-    let { inputId, value, label, changeValue, clearInput } = this.props;
+    let { inputId, value, label, changeValue } = this.props;
     let labelId = inputId ? inputId : Helper.getUuid();
     return (
       <React.Fragment>
@@ -41,21 +39,10 @@ class SearchInput extends React.Component {
         <span className="icon icon_search">
           <i class="fas fa-search"></i>
         </span>
-        <SearchInputClear clearInput={clearInput} value={value} />
-        {/* input type을 search로 사용했을 경우 아래 마크업을 사용*/}
-        {/* <input
-          id={labelId}
-          type="search"
-          className="form_tag"
+        <SearchInputClear
+          clearInput={() => changeValue({ target: { value: '' } })}
           value={value}
-          onChange={changeValue}
         />
-        <label className="f_label" for={labelId}>
-          {label}
-        </label>
-        <span className="icon icon_search">
-          <i class="fas fa-search"></i>
-        </span> */}
       </React.Fragment>
     );
   }
